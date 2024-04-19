ffe3a6e5453b00c716de179b64f33486d4c09b0e
dilanSA132 <121054231+dilanSA132@users.noreply.github.com>
2024-04-19 00:03:28 -0600

    [ADD]Loading storage skeleton , and skeleton component

--- nombre-del-proyecto/src/main.tsx
+++ nombre-del-proyecto/src/main.tsx
@@ -1,19 +1,28 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './styles/index.css'
-import Header  from './componentes/header.tsx'
-import IaButton from './componentes/IaButton.tsx'
-import PromptText from './componentes/prompsText.tsx'
-import TypesButtons from './componentes/typesButton.tsx'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import useButtonStore from './stores/buttonStore';
+import Header from './componentes/header.tsx';
+import IaButton from './componentes/IaButton.tsx';
+import PromptText from './componentes/prompsText.tsx';
+import './styles/index.css';
+import TypesButtons from './componentes/typesButton.tsx';
+import SkeletonHeader from './utils/skeletonHeader.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-     <Header />
-    <div className="flex flex-col items-center justify-start h-screen mt-8">
-      <IaButton />
-      <PromptText />
-      <TypesButtons />
-    </div>
-   
-  </React.StrictMode>,
-)
+const App = () => {
+  const isLoading = useButtonStore((state) => state.isLoading);
+
+  return (
+    <React.StrictMode>
+ 
+      <Header />
+      {isLoading && <SkeletonHeader />}
+      <div className="flex flex-col items-center justify-start h-screen mt-8 ml-4">
+        {!isLoading && <IaButton />}
+        {!isLoading && <PromptText />}
+        {!isLoading && <TypesButtons />}
+      </div>
+    </React.StrictMode>
+  );
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- nombre-del-proyecto/src/stores/buttonStore.ts
+++ nombre-del-proyecto/src/stores/buttonStore.ts
@@ -2,12 +2,16 @@ import create from 'zustand';
 
 interface ButtonStore {
   selectedButton: string | null;
+  isLoading: boolean; 
   setSelectedButton: (buttonName: string) => void;
+  setIsLoading: (loading: boolean) => void; 
 }
 
 const useButtonStore = create<ButtonStore>((set) => ({
   selectedButton: null,
+  isLoading: false, 
   setSelectedButton: (buttonName: string) => set({ selectedButton: buttonName }),
+  setIsLoading: (loading: boolean) => set({ isLoading: loading }), 
 }));
 
 export default useButtonStore;
